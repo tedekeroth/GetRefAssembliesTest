da9653865807ee57497f1efe121426cdf9802ceb
tedekeroth <ted.ekeroth@gmail.com>
2021-08-19 08:43:49 +0200

    Add files via upload

---
+++ GetRefAssembliesTest/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Assembly.GetEntryAssembly().GetReferencedAssemblies();
+            Console.WriteLine($"Printing results from GetReferencedAssemblies():{Environment.NewLine}{string.Join(", ", list.Select(a => a.Name)) }");
+        }
+    }
+}
